83f07a2ff6e98b223ef9d2ecf62b806d83cb83ea
Michael Ciarlo <mciarlo@MacBook-Air.local>
2016-04-27 18:50:41 -0400

    Site finished

--- js/mciarlo.js
+++ js/mciarlo.js
@@ -1,18 +1,12 @@
 $(function () {
 	var $window = $(window),
-		$body = $('body'),
-		windowWidth = $window.outerWidth(),
-		windowHeight = $window.outerHeight(),
 		imageActivationOffset,
 		WIDTH_TO_DISABLE_EFFECTS = 768,
-		IMAGE_ACTIVATION_PERCNTAGE = 0.0,
-		MAX_ILLUSTRATION_HERO_1_ROTATION_ANGLE = 4,
-		START_ILLUSTRATION_HERO_1_ROTATION_ANGLE = 5,
-		MAX_ILLUSTRATION_HERO_2_ROTATION_ANGLE = -5,
-		START_ILLUSTRATION_HERO_2_ROTATION_ANGLE = -5;
+		IMAGE_ACTIVATION_PERCNTAGE = 0.5,
+		SCROLL_ANIMATION_DURATION = 250;
 
 	imageActivationOffset = function () {
-		return $window.outerWidth() < WIDTH_TO_DISABLE_EFFECTS ? 0 : - ($window.height() * IMAGE_ACTIVATION_PERCNTAGE);
+		return $window.height() * IMAGE_ACTIVATION_PERCNTAGE;
 	};
 
 	// Enable our hamburger menu for mobile
@@ -23,7 +17,43 @@ $(function () {
 		$('#nav-menu').toggleClass('active');
 	});
 
-	if ($('body').hasClass('contact')) {
+	// Support Jumper
+	$('.back-to-top').click(function (ev) {
+		ev.preventDefault();
+		$("body, html").animate({'scrollTop' :  0});
+	});
+
+	var numberOfFrames = 8,
+		$video = $('#resize-video'),
+		scrollTimeout,
+		WINDOW_PERCENTAGE_FOR_ANIMATION_START = 0.5,
+		WINDOW_PERCENTAGE_FOR_ANIMATION_DURATION = 0.5;
+
+	if ($video.length > 0) {
+		function scrollPlay() {
+			if (scrollTimeout) {
+				clearTimeout(scrollTimeout);
+			}
+
+			var distanceForWindow = $window.outerHeight() * WINDOW_PERCENTAGE_FOR_ANIMATION_DURATION;
+
+			scrollTimeout = setTimeout(function () {
+				var diff = $video.offset().top - window.pageYOffset - ($window.outerHeight() * WINDOW_PERCENTAGE_FOR_ANIMATION_START);
+
+			  	var framesPercentage = diff < 0 ? Math.abs(diff) / distanceForWindow : 0,
+			  		sanitizedPercentage = framesPercentage > 1 ? 1 : framesPercentage,
+			  		activeFrame = Math.floor(numberOfFrames * sanitizedPercentage);
+
+			  		console.log(activeFrame);
+				
+				$video.removeClass().addClass("frame" + activeFrame);
+			}, 14);
+		}
+
+		$window.scroll(scrollPlay);
+	}
+
+	if ($("#subject-select").length > 0) {
 		$("#subject-select").change(function () {
 			$("#subject-input").attr('value', $("#subject-select option:selected").text());
 		});
@@ -33,6 +63,9 @@ $(function () {
 		});
 
 		$("#support-submit").click(function (ev) {
+			ev.preventDefault();
+			ev.stopPropagation();
+
 			$("#name-input, #email-input, #message-input").removeClass('required');
 			
 			if ($("#name-input").val().length == 0 ||
@@ -50,23 +83,18 @@ $(function () {
 				if ($("#message-input").val().length == 0) {
 					$("#message-input").addClass('required');
 				}
-
-				ev.preventDefault();
-				ev.stopPropagation();
 			}
-
 		});
-
-		return;
 	}
 
-	if ($('body').hasClass('faq')) {
+	if ($(".faq-link").length > 0 || $(".support-topic-filter").length > 0) {
 		$(".faq-link").click(function (ev) {
 			ev.preventDefault();
 			
 			var el = $(this).attr('href'), $el = $(el), multiplier = $window.outerWidth() < 600 ? .05 : .3;
-		    $("body, html").animate({'scrollTop' :  $el.offset().top - ($window.outerHeight() * multiplier)}, 200, function () {
+		    $("body, html").animate({'scrollTop' :  $el.offset().top - ($window.outerHeight() * multiplier)}, SCROLL_ANIMATION_DURATION, function () {
 		    	$el.addClass('highlight');
+		    	window.location.hash = el;
 
 		    	setTimeout(function () {
 		    		$el.removeClass('highlight');
@@ -74,77 +102,56 @@ $(function () {
 		    });
 		});
 
-		return;
+		$(".support-topic-filter").click(function (ev) {
+			ev.preventDefault();
+			$(".support-topic-filter").parent().removeClass('active');
+			$(this).parent().addClass('active');
+
+			var el = $(this).attr('href'), $el = $(el), multiplier = 0.03;
+
+			$(".support-topic-list").hide();
+			$el.show();
+
+		    $("body, html").animate({'scrollTop' :  $el.offset().top - ($window.outerHeight() * multiplier)}, SCROLL_ANIMATION_DURATION, function () {
+		    	$el.addClass('highlight');
+		    	window.location.hash = el;
+
+		    	setTimeout(function () {
+		    		$el.removeClass('highlight');
+		    	}, 1200);
+		    });
+		});
+
+		$(".topic-cancel-filter").click(function (ev) {
+			ev.preventDefault();
+
+			var el = $(this).attr('href'), $el = $(el);
+			$(this).parent().removeClass('active');
+			$(".support-topic-list").show();
+		});
 	}
 
-	// Prepare text blocks for animation
-	$(".details").addClass('invisible');
+	if ($('img.lazy-load').length > 0) {
+		// Prepare text blocks for animation
+		$(".details").addClass('invisible');
+		$('.details').trigger("unveil");
+		$("#conclusion-hero").trigger("unveil");
 
-	$("#play-btn").click(function (ev) {
-		ev.preventDefault();
-		$("#intro-video").toggleClass('active');
+		// Lazy load major image assets
+	  	$("img.lazy-load").removeClass('hidden').addClass('invisible').unveil(imageActivationOffset(), function() {
+	  		if (this.getAttribute("data-src").indexOf("macbook_icloud") > 0) {
+	  			$('#icloud-1, #icloud-2').trigger("unveil");
+	  		}
 
-	    var activeVideo = document.getElementById("video-element");
-	    activeVideo.currentTime = 0;
-	    activeVideo.play();
-	});
-
-	$("#close-video-btn").click(function (ev) {
-		ev.preventDefault();
-		
-		$("#intro-video").removeClass('active');
-		var activeVideo = document.getElementById("video-element");
-	    activeVideo.pause();
-	});
-
-	// Lazy load major image assets
-  	$("img.lazy-load").removeClass('hidden').addClass('invisible').unveil(imageActivationOffset(), function() {
-  		if (this.getAttribute("data-src").indexOf("ipad_icloud_screenshot") > 0) {
-  			$('#icloud-1').trigger("unveil");
-  		}
-
-  		if (this.getAttribute("data-src").indexOf("doo_complexity_reminder_screenshot") > 0) {
-  			setTimeout(function () {
-				$(".details").removeClass('invisible');
-  			}, 100 * 3);
-  		}
-  		
-	  	$(this).load(function() {
-	    	$(this).removeClass('invisible');
-	  	});
-	});
-
-	  			$('img.lazy-load').trigger("unveil");
-
-	  			$('.details').trigger("unveil");
-
-	$("#conclusion-hero").trigger("unveil");
-
-	// Animate out illustration heros during scroll
-	$("#illustration-hero-1").imageDynamics(function() {
-		$("#illustration-hero-2").trigger("unveil");
-	  	var percentageInView = ($window.scrollTop() - $(this).offset().top) / $window.height();
-	  	var angle = START_ILLUSTRATION_HERO_1_ROTATION_ANGLE - (percentageInView * MAX_ILLUSTRATION_HERO_1_ROTATION_ANGLE);
-
-	   	$(this).trigger("unveil").css({
-	   		'transform'			: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-moz-transform'	: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-webkit-transform'	: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-o-transform'		: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-ms-transform'		: 'rotate3d(0, 0, 1, ' + angle + 'deg)'
-	   	});
-	});
-
-	$("#illustration-hero-2").imageDynamics(function() {
-	  	var percentageInView = ($window.scrollTop() - $(this).offset().top) / $window.height();
-	  	var angle = START_ILLUSTRATION_HERO_2_ROTATION_ANGLE - (percentageInView * MAX_ILLUSTRATION_HERO_2_ROTATION_ANGLE);
-
-	   	$(this).css({
-	   		'transform'			: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-moz-transform'	: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-webkit-transform'	: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-o-transform'		: 'rotate3d(0, 0, 1, ' + angle + 'deg)',
-	   		'-ms-transform'		: 'rotate3d(0, 0, 1, ' + angle + 'deg)'
-	   	});
-	});
+	  		if (this.getAttribute("data-src").indexOf("doo_complexity_reminder_screenshot") > 0) {
+	  			setTimeout(function () {
+					$(".details").removeClass('invisible');
+	  			}, 100 * 3);
+	  		}
+	  		
+		  	$(this).load(function() {
+		    	$(this).removeClass('invisible');
+		  	});
+		});
+	}
 });
